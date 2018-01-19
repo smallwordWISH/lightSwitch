5b9d39dc2c10dc7e92cd4ecf6b87f956732181d3
smallwordWISH <word714cs@gmail.com>
2018-01-19 17:19:35 +0800

    addinput timer function and modify css

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
 $(document).ready(function () {
 
-  var state = true;
-
   $('.button').click(function() { 
-    state = !state;
-    imageSrc = state ? "img/on.jpg" : "img/off.jpg ";
-    $('#img').attr('src', imageSrc);
-  });
-
-  var timeLeft = 5;
-  var interval = setInterval(countDownTimer, 1000);
-
-  function countDownTimer() {
-    timeLeft -= 1;
-
-    if (timeLeft <= 0) {
-      $('#img').attr('src', 'img/off.jpg');
-      clearInterval(interval);
-      state = !state;
+    if ($('#img').attr('src') == "img/on.jpg"){
+      $('#img').attr('src', "img/off.jpg");
+    } else {
+      $('#img').attr('src', "img/on.jpg");
     }
+  }); // end click
 
+  $('#timer-btn').click(function(){
+    var timeLeft = $('#text').val();
+    if (timeLeft == "") {
+      alert("請填入數字");
+      return;
+    } else if (isNaN(timeLeft)) {
+      alert("請填入數字");
+      return;
+    }
+    timeLeft = parseInt(timeLeft);
+    $('#text').val('');
+    $('body button').prop('disabled', true);
+
+    $('#img').attr('src', "img/on.jpg");
     $('.timer').html(timeLeft);
-  }
 
+    var interval = setInterval(countDownTimer, 1000);
+
+    function countDownTimer() {
+      timeLeft -= 1;
+      if (timeLeft <= 0) {
+        $('#img').attr('src', 'img/off.jpg');
+        clearInterval(interval);
+        $('body button').prop('disabled', false);
+      }
+
+      $('.timer').html(timeLeft); 
+    }
+  }); // end click
 })
